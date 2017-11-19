b8141c11fec5469db7c8a125fc2b4766db367ae1
Dinesh <dinesh.majeti@gmail.com>
2017-11-19 13:51:21 -0600

    display badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2448501.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      console.log(response);
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(data) {
+  	$badges = $('#badges');
+
+  	data.forEach(function(course) {
+
+  		$div = $('<div />', {
+  			'class' : 'course'
+  		}).appendTo($badges);
+
+  		$('<h3 />', {
+  			'text': course.title
+  		}).appendTo($div);
+
+  		$('<img />', {
+  			'src': course.badge
+  		}).appendTo($div);
+
+  		$('<a />', {
+  			'href': course.url,
+  			'target': '_blank',
+  			'class': 'btn btn-primary',
+  			'text': 'See Course'
+  		}).appendTo($div);
+  	});
+  }
+
 
 });
